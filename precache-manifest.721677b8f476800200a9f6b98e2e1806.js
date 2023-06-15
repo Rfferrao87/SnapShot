7d7dc310b65279873b6ae7e6e1496d6d658c7c2d
Ramiro Fróes Ferrão <rfferrao@nebrasil.com.br>
2023-06-15 02:35:46 -0300

    Updates

---
+++ precache-manifest.721677b8f476800200a9f6b98e2e1806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d78f4b5272115ad723de31bdadf033",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "082415e8d98595c75d39",
+    "url": "/SnapShot/static/css/main.ba1c0e40.chunk.css"
+  },
+  {
+    "revision": "b736583c24d6bf3cca7a",
+    "url": "/SnapShot/static/js/2.55b88fe0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SnapShot/static/js/2.55b88fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082415e8d98595c75d39",
+    "url": "/SnapShot/static/js/main.1c26cf4d.chunk.js"
+  },
+  {
+    "revision": "5d8ea8bbc63e642a234c",
+    "url": "/SnapShot/static/js/runtime-main.7774f14f.js"
+  }
+]);
